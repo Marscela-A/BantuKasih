fbeae567618d171a4caeb3ee8fbb019bba7707ad
Marscela-A <marscelaaurora@gmail.com>
2024-12-11 15:55:36 +0700

    update navbar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\HTTP
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -9,5 +9,3 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('menu.home');
 });
-Route::get('/tentang-kami', [aboutusController::class, 'index'])->name('tentang-kami')
-Route::get
