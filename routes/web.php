950a2b354c9a854a3def40e544dfe9956a230589
santiasann <santiasann@gmail.com>
2024-12-11 15:27:38 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\HTTP
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('menu.home');
-});
-Route::get('/tentang-kami', [aboutusController::class, 'index'])->name('tentang-kami')
-Route::get
+
