5a9477b14ea6b70580b9eda8ce92dff81375be6a
tvaniac <vaniacarissa398@gmail.com>
2024-12-23 11:20:11 +0700

    pembayaran nih

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -31,5 +31,7 @@
     </div>
     @include('layout.footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    
+    @yield('scripts') 
 </body>
 </html>
